f338b4541f3615d08012a5a462bbefb0cb4eba04
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-01-24 18:19:54 +0100

    Update MainController.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -13,42 +13,27 @@ class MainController extends Controller
 {
     public function catalog(Request $request, Catalog $catalog)
     {
-        $h1 = null;
-        $h2 = null;
-        $meta_description = null;
-
-        if (url()->current() == route('woman.catalog') && Catalog::where("gender", "Femme")->first()) {
-            $catalog = Catalog::where("gender", "Femme")->first()->id;
-            $h1 = "Femme";
-            $h2 = "Découvrez notre collection féminine : élégance, style et confiance !";
-            $meta_description = "Découvrez notre collection de prêt-à-porter pour hommes. Trouvez des vêtements tendance, élégants et de haute qualité pour compléter votre style.";
-        } else if (url()->current() == route('men.catalog') && Catalog::where("gender", "Homme")->first()) {
-            $catalog = Catalog::where("gender", "Homme")->first()->id;
-            $h1 = "Homme";
-            $h2 = "Découvrez notre collection masculine : élégance, sophistication et confiance !";
-            $meta_description = "Découvrez notre collection de prêt-à-porter pour femmes. Trouvez des vêtements tendance, élégants et de haute qualité pour compléter votre style.";
-        }
-
+        $query = ("/", url()->current(), 2);
+        $catalog_id = Catalog::where("gender", $query[1])->first()->id;
+        $categories = Category::where("catalog_id", $catalog_id)->get();
+        
         switch ($request->filter) {
             case "new":
-                $products = Product::where("catalog_id", $catalog)->orderBy('created_at', 'ASC')->paginate(12);
+                $products = Product::where("catalog_id", $catalog_id)->orderBy('created_at', 'ASC')->paginate(12);
                 break;
             case "price-lowest":
-                $products = Product::where("catalog_id", $catalog)->orderBy('price', 'ASC')->paginate(12);
+                $products = Product::where("catalog_id", $catalog_id)->orderBy('price', 'ASC')->paginate(12);
                 break;
             case "price-highest":
-                $products = Product::where("catalog_id", $catalog)->orderBy('price', 'DESC')->paginate(12);
+                $products = Product::where("catalog_id", $catalog_id)->orderBy('price', 'DESC')->paginate(12);
                 break;
             default:
-                $products = Product::where("catalog_id", $catalog)->orderBy('created_at', 'ASC')->paginate(12);
+                $products = Product::where("catalog_id", $catalog_id)->orderBy('created_at', 'ASC')->paginate(12);
         }
 
         return view('products/catalog', [
             "products" => $products,
-            "h1" => $h1,
-            "h2" => $h2,
-            "categories" => Category::where("catalog_id", $catalog)->get(),
-            "meta_description" => $meta_description
+            "categories" => $categories,
         ]);
     }
 
