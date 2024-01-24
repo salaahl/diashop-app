c33738f64371601eb14022a76e409e311cab5ce1
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-01-24 18:54:46 +0100

    Update catalog.blade.php

--- resources/views/products/catalog.blade.php
+++ resources/views/products/catalog.blade.php
@@ -1,14 +1,14 @@
 @php
-$query = explode("/", url()->current())
-@if($query[2] == "woman")
-$h1 = "Femme";
-$h2 = "Découvrez notre collection féminine : élégance, style et confiance !";
-$meta_description = "Découvrez notre collection de prêt-à-porter pour femmes. Trouvez des vêtements tendance, élégants et de haute qualité pour compléter votre style.";
-@elseif($query[2] == "men")
-$h1 = "Homme";
-$h2 = "Découvrez notre collection masculine : élégance, sophistication et confiance !";
-$meta_description = "Découvrez notre collection de prêt-à-porter pour hommes. Trouvez des vêtements tendance, élégants et de haute qualité pour compléter votre style.";
-@endif
+$query = explode("/", url()->current());
+if($query[2] == "woman") {
+    $h1 = "Femme";
+    $h2 = "Découvrez notre collection féminine : élégance, style et confiance !";
+    $meta_description = "Découvrez notre collection de prêt-à-porter pour femmes. Trouvez des vêtements tendance, élégants et de haute qualité pour compléter votre style.";
+} elseif($query[2] == "men") {
+    $h1 = "Homme";
+    $h2 = "Découvrez notre collection masculine : élégance, sophistication et confiance !";
+    $meta_description = "Découvrez notre collection de prêt-à-porter pour hommes. Trouvez des vêtements tendance, élégants et de haute qualité pour compléter votre style.";
+}
 @endphp
 
 @extends('layouts.app')
