4013de3c6cda857fb8059330e9ecc9a216f0ce83
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-01-24 18:21:37 +0100

    Update catalog.blade.php

--- resources/views/products/catalog.blade.php
+++ resources/views/products/catalog.blade.php
@@ -1,3 +1,16 @@
+@php
+$query = ("/", url()->current(), 2)
+@if($query[1] == "femme")
+$h1 = "Femme";
+$h2 = "Découvrez notre collection féminine : élégance, style et confiance !";
+$meta_description = "Découvrez notre collection de prêt-à-porter pour femmes. Trouvez des vêtements tendance, élégants et de haute qualité pour compléter votre style.";
+@elseif($query[1] == "homme")
+$h1 = "Homme";
+$h2 = "Découvrez notre collection masculine : élégance, sophistication et confiance !";
+$meta_description = "Découvrez notre collection de prêt-à-porter pour hommes. Trouvez des vêtements tendance, élégants et de haute qualité pour compléter votre style.";
+@endif
+@endphp
+
 @extends('layouts.app')
 
 @section('meta')
