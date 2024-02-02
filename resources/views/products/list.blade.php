4eed2ca18b3d86270347db25f673e176aea15e03
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-02-02 17:00:53 +0100

    Update list.blade.php

--- resources/views/products/list.blade.php
+++ resources/views/products/list.blade.php
@@ -41,7 +41,7 @@ $meta_description = "Découvrez notre collection de prêt-à-porter pour hommes.
 @if($categories)
 <div id="categories" class="flex w-full my-8 overflow-x-auto">
     @foreach($categories as $category)
-    <article class="category">
+    <article class="category" @if(isset($query[5])) @if($query[5] == $category->name) selected @endif @endif>
         <a href="/{{ $query[3] }}/catalog/{{ $category->name }}">
             <div class="thumbnail">
                 <img src='{{ asset("/images/$category->img_thumbnail") }}' />
