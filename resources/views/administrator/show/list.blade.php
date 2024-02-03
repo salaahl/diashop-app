b2d5f5e6a8fedd8921bb1b005fa759c5af6a29f5
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-02-03 12:18:14 +0100

    Update list.blade.php

--- resources/views/administrator/show/list.blade.php
+++ resources/views/administrator/show/list.blade.php
@@ -8,11 +8,6 @@
 
 @section('links')
 @parent
-<style>
-    .button-stylised-1, button-stylised-1-custom {
-        width: auto;
-    }
-</style>
 @endsection
 
 @section('header')
@@ -26,9 +21,9 @@
     <h3>{{ $catalog->gender }}</h3>
     <div class="w-screen lg:w-auto flex justify-between">
         <a href="../edit/catalog/{{ $catalog->id }}" class="button-stylised-1 lg:min-w-[10vw]">Modifier</a>
-        <form method="POST" action="../delete/catalog/{{ $catalog->id }}" onSubmit="return confirm('Etes-vous sûr ? Cette action est irreversible')">
+        <form method="POST" action="../delete/catalog/{{ $catalog->id }}" class="min-w-[45vw] lg:min-w-[10vw]" onSubmit="return confirm('Etes-vous sûr ? Cette action est irreversible')">
             @csrf
-            <button role="submit" class="button-stylised-1 button-stylised-1-custom">Supprimer</button>
+            <button role="submit" class="button-stylised-1 button-stylised-1-custom w-full">Supprimer</button>
         </form>
     </div>
 </li>
@@ -39,9 +34,9 @@
     <h3>{{ $category->name }}</h3>
     <div class="w-screen lg:w-auto flex justify-between">
         <a href="../edit/category/{{ $category->id }}" class="button-stylised-1 min-w-[45vw] lg:min-w-[10vw]">Modifier</a>
-        <form method="POST" action="../delete/category/{{ $category->id }}" onSubmit="return confirm('Etes-vous sûr ? Cette action est irreversible')">
+        <form method="POST" action="../delete/category/{{ $category->id }}" class="min-w-[45vw] lg:min-w-[10vw]" onSubmit="return confirm('Etes-vous sûr ? Cette action est irreversible')">
             @csrf
-            <button role="submit" class="button-stylised-1 button-stylised-1-custom">Supprimer</button>
+            <button role="submit" class="button-stylised-1 button-stylised-1-customw-full">Supprimer</button>
         </form>
     </div>
 </li>
@@ -52,9 +47,9 @@
     <h3>{{ $product->name }}</h3>
     <div class="w-screen lg:w-auto flex justify-between">
         <a href="../edit/product/{{ $product->id }}" class="button-stylised-1 min-w-[45vw] lg:min-w-[10vw]">Modifier</a>
-        <form method="POST" action="../delete/product/{{ $product->id }}" onSubmit="return confirm('Etes-vous sûr ? Cette action est irreversible')">
+        <form method="POST" action="../delete/product/{{ $product->id }}" class="min-w-[45vw] lg:min-w-[10vw]" onSubmit="return confirm('Etes-vous sûr ? Cette action est irreversible')">
             @csrf
-            <button role="submit" class="button-stylised-1 button-stylised-1-custom">Supprimer</button>
+            <button role="submit" class="button-stylised-1 button-stylised-1-custom w-full">Supprimer</button>
         </form>
     </div>
 </li>
