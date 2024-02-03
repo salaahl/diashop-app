10a92cf326f15556ab8db364cc3c9bc67920978b
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-02-03 13:15:34 +0100

    Update list.blade.php

--- resources/views/administrator/show/list.blade.php
+++ resources/views/administrator/show/list.blade.php
@@ -16,7 +16,12 @@
 
 @section('main')
 @if(isset($catalogs))
-<h1>Catalogues</h1>
+<h1 class="my-20">Catalogues</h1>
+@elseif(isset($categories))
+<h1 class="my-20">Catégories</h1>
+@elseif(isset($products))
+<h1 class="my-20">Articles</h1>
+@endif
 <nav id="filters" class="w-full flex justify-between items-center p-2 my-2 bg-gray-100 rounded-t-lg">
     <h4>Trier par :</h4>
     <select id="filter_select" class="block py-2.5 px-0 text-sm text-gray-500 bg-transparent border-0 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
@@ -26,6 +31,7 @@
         <option value="created-at-desc" @if(request()->get('filter') == 'created-at-desc') selected @endif>Du plus ancien au plus récent</option>
     </select>
 </nav>
+@if(isset($catalogs))
 @foreach($catalogs as $catalog)
 <li class="flex flex-wrap justify-between items-center mb-2 p-2 rounded-lg bg-gray-200">
     <div class="w-screen lg:w-auto flex justify-between">
@@ -41,11 +47,10 @@
 </li>
 @endforeach
 @elseif(isset($categories))
-<h1>Catégories</h1>
 @foreach($categories as $category)
 <li class="flex flex-wrap justify-between items-center mb-2 p-2 rounded-lg bg-gray-200">
     <div class="w-screen lg:w-auto flex justify-between">
-        <h3>{{ ucfirst($category->name) }} - </h3>
+        <h3 class="mr-1">{{ ucfirst($category->name) }} -</h3>
         <h3>{{ ucfirst($category->catalog->gender) }}</h3>
     </div>
     <div class="w-screen lg:w-auto flex justify-between">
@@ -58,12 +63,11 @@
 </li>
 @endforeach
 @elseif(isset($products))
-<h1>Articles</h1>
 @foreach($products as $product)
 <li class="flex flex-wrap justify-between items-center mb-2 p-2 rounded-lg bg-gray-200">
     <div class="w-screen lg:w-auto flex justify-between">
-        <h3>{{ ucfirst($product->name) }} - </h3>
-        <h3>{{ ucfirst($product->catalog->gender) }} - </h3>
+        <h3 class="mr-1">{{ ucfirst($product->name) }} -</h3>
+        <h3 class="mr-1">{{ ucfirst($product->catalog->gender) }} -</h3>
         <h3>{{ ucfirst($product->category->name) }}</h3>
     </div>
     <div class="w-screen lg:w-auto flex justify-between">
