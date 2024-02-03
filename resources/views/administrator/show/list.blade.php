13901d5668843fc8683b687a3f905f742c07af89
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-02-03 12:06:26 +0100

    Update list.blade.php

--- resources/views/administrator/show/list.blade.php
+++ resources/views/administrator/show/list.blade.php
@@ -22,10 +22,10 @@
 @section('main')
 @if(isset($catalogs))
 @foreach($catalogs as $catalog)
-<li class="flex justify-between">
+<li class="flex flex-wrap justify-between">
     <h3>{{ $catalog->gender }}</h3>
-    <div class="flex">
-        <a href="../edit/catalog/{{ $catalog->id }}" class="button-stylised-1 lg:min-w-[10%]">Modifier</a>
+    <div class="w-screen lg:w-auto flex justify-between">
+        <a href="../edit/catalog/{{ $catalog->id }}" class="button-stylised-1 lg:min-w-[10vw]">Modifier</a>
         <form method="POST" action="../delete/catalog/{{ $catalog->id }}" onSubmit="return confirm('Etes-vous sûr ? Cette action est irreversible')">
             @csrf
             <button role="submit" class="button-stylised-1 button-stylised-1-custom">Supprimer</button>
@@ -35,10 +35,10 @@
 @endforeach
 @elseif(isset($categories))
 @foreach($categories as $category)
-<li class="flex justify-between">
+<li class="flex flex-wrap justify-between">
     <h3>{{ $category->name }}</h3>
-    <div class="flex">
-        <a href="../edit/category/{{ $category->id }}" class="button-stylised-1 lg:min-w-[10%]">Modifier</a>
+    <div class="w-screen lg:w-auto flex justify-between">
+        <a href="../edit/category/{{ $category->id }}" class="button-stylised-1 min-w-[45vw] lg:min-w-[10vw]">Modifier</a>
         <form method="POST" action="../delete/category/{{ $category->id }}" onSubmit="return confirm('Etes-vous sûr ? Cette action est irreversible')">
             @csrf
             <button role="submit" class="button-stylised-1 button-stylised-1-custom">Supprimer</button>
@@ -48,10 +48,10 @@
 @endforeach
 @elseif(isset($products))
 @foreach($products as $product)
-<li class="flex justify-between">
+<li class="flex flex-wrap justify-between">
     <h3>{{ $product->name }}</h3>
-    <div class="flex">
-        <a href="../edit/product/{{ $product->id }}" class="button-stylised-1 lg:min-w-[10%]">Modifier</a>
+    <div class="w-screen lg:w-auto flex justify-between">
+        <a href="../edit/product/{{ $product->id }}" class="button-stylised-1 min-w-[45vw] lg:min-w-[10vw]">Modifier</a>
         <form method="POST" action="../delete/product/{{ $product->id }}" onSubmit="return confirm('Etes-vous sûr ? Cette action est irreversible')">
             @csrf
             <button role="submit" class="button-stylised-1 button-stylised-1-custom">Supprimer</button>
