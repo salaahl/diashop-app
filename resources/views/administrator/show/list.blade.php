301cd5d9e876794ed1f6a1c6fa9bc25c3257f46e
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-02-03 11:53:32 +0100

    Update list.blade.php

--- resources/views/administrator/show/list.blade.php
+++ resources/views/administrator/show/list.blade.php
@@ -8,6 +8,11 @@
 
 @section('links')
 @parent
+<style>
+    .button-stylised-1 {
+        width: auto;
+    }
+</style>
 @endsection
 
 @section('header')
@@ -17,35 +22,41 @@
 @section('main')
 @if(isset($catalogs))
 @foreach ($catalogs as $catalog)
-<li class="flex">
+<li class="flex justify-between">
     <h3>{{ $catalog->name }}</h3>
-    <a href="../edit/catalog/{{ $catalog->id }}" class="button-stylised-1">Modifier</a>
-    <form method="POST" action="../delete/catalog/{{ $catalog->id }}" onSubmit="return confirm('Etes-vous sûr ? Cette action est irreversible')">
-        @csrf
-        <button role="submit" class="button-stylised-1 button-stylised-1-custom">Supprimer</button>
-    </form>
+    <div class="flex">
+        <a href="../edit/catalog/{{ $catalog->id }}" class="button-stylised-1">Modifier</a>
+        <form method="POST" action="../delete/catalog/{{ $catalog->id }}" onSubmit="return confirm('Etes-vous sûr ? Cette action est irreversible')">
+            @csrf
+            <button role="submit" class="button-stylised-1 button-stylised-1-custom">Supprimer</button>
+        </form>
+    </div>
 </li>
 @endforeach
 @elseif(isset($categories))
 @foreach ($categories as $category)
 <li class="flex">
     <h3>{{ $category->name }}</h3>
-    <a href="../edit/category/{{ $category->id }}" class="button-stylised-1">Modifier</a>
-    <form method="POST" action="../delete/category/{{ $category->id }}" onSubmit="return confirm('Etes-vous sûr ? Cette action est irreversible')">
-        @csrf
-        <button role="submit" class="button-stylised-1 button-stylised-1-custom">Supprimer</button>
-    </form>
+    <div class="flex">
+        <a href="../edit/category/{{ $category->id }}" class="button-stylised-1">Modifier</a>
+        <form method="POST" action="../delete/category/{{ $category->id }}" onSubmit="return confirm('Etes-vous sûr ? Cette action est irreversible')">
+            @csrf
+            <button role="submit" class="button-stylised-1 button-stylised-1-custom">Supprimer</button>
+        </form>
+    </div>
 </li>
 @endforeach
 @elseif(isset($products))
 @foreach ($products as $product)
 <li class="flex">
     <h3>{{ $product->name }}</h3>
-    <a href="../edit/product/{{ $product->id }}" class="button-stylised-1">Modifier</a>
-    <form method="POST" action="../delete/product/{{ $product->id }}" onSubmit="return confirm('Etes-vous sûr ? Cette action est irreversible')">
-        @csrf
-        <button role="submit" class="button-stylised-1 button-stylised-1-custom">Supprimer</button>
-    </form>
+    <div class="flex">
+        <a href="../edit/product/{{ $product->id }}" class="button-stylised-1">Modifier</a>
+        <form method="POST" action="../delete/product/{{ $product->id }}" onSubmit="return confirm('Etes-vous sûr ? Cette action est irreversible')">
+            @csrf
+            <button role="submit" class="button-stylised-1 button-stylised-1-custom">Supprimer</button>
+        </form>
+    </div>
 </li>
 @endforeach
 @endif
