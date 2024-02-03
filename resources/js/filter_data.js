44aa01c1d86b1ab12ea1b6a730b522605cb1f6d7
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-02-03 15:16:07 +0100

    Update filter_data.js

--- resources/js/filter_data.js
+++ resources/js/filter_data.js
@@ -1,9 +1,9 @@
 document.getElementById("filter_select").addEventListener("change", () => {
-    var myform = document.createElement("form");
+    let myform = document.createElement("form");
     myform.action = "";
     myform.method = "post";
     
-    filter = document.createElement("input");
+    let filter = document.createElement("input");
     filter.value = document.getElementById("filter_select").value;
     filter.name = "filter";
     
