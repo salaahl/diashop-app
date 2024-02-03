0c0b4e9df6b74319847b3bdad8ede73eda60ed4f
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-02-03 12:58:06 +0100

    Update list.blade.php

--- resources/views/administrator/show/list.blade.php
+++ resources/views/administrator/show/list.blade.php
@@ -17,6 +17,15 @@
 @section('main')
 @if(isset($catalogs))
 <h1>Catalogues</h1>
+<nav id="filters" class="w-full flex justify-between items-center p-2 my-2 bg-gray-100 rounded-t-lg">
+    <h4>Trier par :</h4>
+    <select id="filter_select" class="block py-2.5 px-0 text-sm text-gray-500 bg-transparent border-0 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
+        <option value="alphabetical-asc" @if(request()->get('filter') == 'alphabetical-asc' || !request()->get('filter')) selected @endif>A-Z</option>
+        <option value="alphabetical-desc" @if(request()->get('filter') == 'alphabetical-desc') selected @endif>Z-A</option>
+        <option value="created-at-asc" @if(request()->get('filter') == 'created-at-asc') selected @endif>Du plus récent au plus ancien</option>
+        <option value="created-at-desc" @if(request()->get('filter') == 'created-at-desc') selected @endif>Du plus ancien au plus récent</option>
+    </select>
+</nav>
 @foreach($catalogs as $catalog)
 <li class="flex flex-wrap justify-between items-center mb-2 p-2 rounded-lg bg-gray-200">
     <div class="w-screen lg:w-auto flex justify-between">
