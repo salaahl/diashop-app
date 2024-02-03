48fbddd049036ff33f13a5ed5d259ba80e9f5639
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-02-03 12:38:16 +0100

    Update list.blade.php

--- resources/views/administrator/show/list.blade.php
+++ resources/views/administrator/show/list.blade.php
@@ -17,7 +17,7 @@
 @section('main')
 @if(isset($catalogs))
 @foreach($catalogs as $catalog)
-<li class="flex flex-wrap justify-between">
+<li class="flex flex-wrap justify-between items-center mb-2 p-1 bg-gray-200">
     <h3>{{ $catalog->gender }}</h3>
     <div class="w-screen lg:w-auto flex justify-between">
         <a href="../edit/catalog/{{ $catalog->id }}" class="button-stylised-1 lg:min-w-[10vw] lg:mr-[40px]">Modifier</a>
@@ -30,20 +30,20 @@
 @endforeach
 @elseif(isset($categories))
 @foreach($categories as $category)
-<li class="flex flex-wrap justify-between">
+<li class="flex flex-wrap justify-between items-center mb-2 p-1 bg-gray-200">
     <h3>{{ $category->name }}</h3>
     <div class="w-screen lg:w-auto flex justify-between">
         <a href="../edit/category/{{ $category->id }}" class="button-stylised-1 min-w-[45vw] lg:min-w-[10vw] lg:mr-[40px]">Modifier</a>
         <form method="POST" action="../delete/category/{{ $category->id }}" class="min-w-[45vw] lg:min-w-[10vw]" onSubmit="return confirm('Etes-vous sûr ? Cette action est irreversible')">
             @csrf
-            <button role="submit" class="button-stylised-1 button-stylised-1-customw-full">Supprimer</button>
+            <button role="submit" class="button-stylised-1 button-stylised-1-custom w-full">Supprimer</button>
         </form>
     </div>
 </li>
 @endforeach
 @elseif(isset($products))
 @foreach($products as $product)
-<li class="flex flex-wrap justify-between">
+<li class="flex flex-wrap justify-between items-center mb-2 p-1 bg-gray-200">
     <h3>{{ $product->name }}</h3>
     <div class="w-screen lg:w-auto flex justify-between">
         <a href="../edit/product/{{ $product->id }}" class="button-stylised-1 min-w-[45vw] lg:min-w-[10vw] lg:mr-[40px]">Modifier</a>
