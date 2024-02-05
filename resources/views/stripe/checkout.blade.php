ba0cc8d6b10d7d9bf9c55e3408e2bf641cd994b7
salaahl <sokhona.salaha@gmail.com>
2024-02-05 21:21:08 +0100

    réintégration du fichier checkout

---
+++ resources/views/stripe/checkout.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('meta')
+@parent
+@endsection
+
+@section('title', 'Payement')
+
+@section('links')
+@parent
+<script src="https://js.stripe.com/v3/"></script>
+<style>
+    main {
+        padding: 0;
+    }
+</style>
+@endsection
+
+@section('header')
+@endsection
+
+@section('main')
+<div id="checkout"></div>
+@endsection
+
+@section('scripts')
+@parent
+@vite('resources/js/stripe/checkout.js')
+@endsection
