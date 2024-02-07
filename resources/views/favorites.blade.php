23ee5f79ea91574526141a53a1bab5fcd5d057dd
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-02-07 17:47:32 +0100

    Update favorites.blade.php

--- resources/views/favorites.blade.php
+++ resources/views/favorites.blade.php
@@ -16,15 +16,19 @@
 @endsection
 
 @section('main')
-<div id="headers">
-    <h1>Mes favoris</h1>
-</div>
+<h1>Mes favoris</h1>
+@if($products)
 @foreach($products as $product)
 <x-product link="{{ route('product', [$product->catalog->gender, $product->category->name, $product->id]) }}" image="/images/{{ $product->img_thumbnail[0] }}" hover="/images/{{ $product->img_thumbnail[1] }}" title="{{ $product->name }}" price="{{ $product->price }}" />
 @endforeach
 <aside class="w-full">
     {{ $products->links() }}
 </aside>
+@else
+<div class="min-h-[90vh] w-[100vw] absolute top-[10vh] left-0 right-0 flex flex-col justify-center items-center px-4">
+    <h2 class="w-3/4 lg:w-2/4 mb-4 p-4 uppercase rounded-full bg-gray-200">Vous n'avez pas de produits dans vos favoris</h2>
+</div>
+@endif
 @endsection
 
 @section('scripts')
