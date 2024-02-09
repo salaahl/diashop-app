8bc31318983e32684e2de5d14769918717fda6c1
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-02-09 16:00:53 +0100

    Create TrackingNumberEmailJob.php

---
+++ app/Jobs/TrackingNumberEmailJob.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TrackingNumberEmail;
+
+class TrackingNumberEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $data;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $email = new TrackingNumberEmail($this->data);
+
+        Mail::to($this->data[0]->email)->send($email);
+    }
+}
