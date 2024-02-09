65f4303c23e45358506849826e04143ab9ec292e
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-02-09 16:36:22 +0100

    Update ConfirmationEmail.php

--- app/Mail/ConfirmationEmail.php
+++ app/Mail/ConfirmationEmail.php
@@ -39,7 +39,7 @@ class ConfirmationEmail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'emails.confirmation-email',
+            view: 'emails.confirmation',
             with: $this->data
         );
     }
