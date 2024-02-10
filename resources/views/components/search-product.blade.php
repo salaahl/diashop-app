bf2c9eceab64b5ef0f8366581db87f79191e2ae9
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-02-10 13:13:43 +0100

    Update and rename search_product.blade.php to search-product.blade.php

--- resources/views/components/search-product.blade.php
+++ resources/views/components/search-product.blade.php
@@ -2,11 +2,11 @@
     <div class="w-full">
         <div class="w-full flex justify-between mt-10">
             <div class="w-[48%] flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
-                <input checked id="bordered-radio-1" type="radio" value="1" name="bordered-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                <input checked id="bordered-radio-1" type="radio" value="2" name="bordered-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                 <label for="bordered-radio-1" class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Catalogue Femme</label>
             </div>
             <div class="w-[48%] flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
-                <input id="bordered-radio-2" type="radio" value="2" name="bordered-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                <input id="bordered-radio-2" type="radio" value="1" name="bordered-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                 <label for="bordered-radio-2" class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Catalogue Homme</label>
             </div>
         </div>
