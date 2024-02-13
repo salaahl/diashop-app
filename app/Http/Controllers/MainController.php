514a85398d733d7d81e2fa06d4d3bc7ddb25fa8d
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-02-13 15:51:41 +0100

    Update MainController.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -12,6 +12,7 @@ class MainController extends Controller
 {
     public function catalog($gender, Request $request)
     {
+        dd($request->session()->all());
         dd(session()->all());
         $catalog_id = Catalog::where("gender", $gender)->first()->id;
         $categories = Category::where("catalog_id", $catalog_id)->get();
