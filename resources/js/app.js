c39909a29a9a24ae72fcd8bcbc724b8d65e4473b
salaahl <sokhona.salaha@gmail.com>
2024-02-14 02:12:02 +0100

    revert et ajout d'une fonction

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,7 @@ window.Alpine = Alpine;
 
 Alpine.start();
 
+// Retracte la barre de navigation en mode desktop
 if (window.innerWidth > 1023) {
     var prevScrollpos = window.scrollY;
     window.onscroll = function () {
@@ -18,6 +19,16 @@ if (window.innerWidth > 1023) {
         prevScrollpos = currentScrollPos;
     };
 }
+
+// Redimentionne dynamiquement la hauteur de la fenêtre. Permet de corriger le souci de viewheight en mode mobile
+function resetHeight() {
+    // reset the body height to that of the inner browser
+    document.body.style.height = window.innerHeight + "px";
+}
+// reset the height whenever the window's resized
+window.addEventListener("resize", resetHeight);
+// called to initially set the height.
+resetHeight();
 
 // Loader des pages
 window.addEventListener("load", () => {
