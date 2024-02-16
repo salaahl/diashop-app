4190b0f300871b64a0c3046dc240d384545e2373
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-02-16 16:15:35 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -36,8 +36,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 
     Route::get('profile/', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('profile/', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('profile/', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('profile/update', [ProfileController::class, 'update'])->name('profile.update');
+    // Route::post('profile/delete', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::get('favorites/', [FavoriteController::class, 'show'])->name('favorites.show');
     Route::put('favorites/add', [FavoriteController::class, 'store'])->name('favorites.store');
