b1949474e8b34b62d9241dcd062be21f8d078a60
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-02-17 11:01:23 +0100

    Update and rename product.blade.php to product-card.blade.php

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -1,8 +1,8 @@
 <article class="product">
     <a href="{{ $link }}">
         <div class="thumbnail">
-            <img src="{{ $image }}" />
-            <img src="{{ $hover }}" />
+            <img src="{{ $image1 }}" />
+            <img src="{{ $image2 }}" />
         </div>
         <div class="details lg:flex justify-between px-1 py-2 mt-1">
             <h4 class="title lg:max-w-[75%] items-center text-ellipsis overflow-hidden">{{ ucfirst($title) }}</h4>
