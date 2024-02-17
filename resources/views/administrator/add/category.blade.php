8953171636eca4122bea8fb2aca2e814009ea73b
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-02-17 11:09:04 +0100

    Update category.blade.php

--- resources/views/administrator/add/category.blade.php
+++ resources/views/administrator/add/category.blade.php
@@ -50,12 +50,12 @@
             </select>
         </div>
         <div class="mb-10">
-            <label for="category" class="block mb-2 text-sm font-medium text-gray-900 uppercase">Catégorie</label>
-            <input type="text" name="category" id="category" minlength="2" maxlength="60" @if(isset($category)) value="{{ $category->name }}" @endif class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Nom de la catégorie (exemple : vestes et manteaux)" required>
+            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 uppercase">Catégorie</label>
+            <input type="text" name="name" id="name" minlength="2" maxlength="60" @if(isset($category)) value="{{ $category->name }}" @endif class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Nom de la catégorie (exemple : vestes et manteaux)" required>
         </div>
         <div>
-            <label class="block mb-2 text-sm font-medium text-gray-900" for="img_thumbnail">MINIATURE (formats acceptés : SVG, PNG ou JPG).</label>
-            <input aria-describedby="img_thumbnail_input_help" id="img_thumbnail" type="file" name="img_thumbnail" accept="image/png, image/jpg, image/jpeg, image/gif" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none" required>
+            <label class="block mb-2 text-sm font-medium text-gray-900" for="img">Image (formats acceptés : SVG, PNG ou JPG).</label>
+            <input aria-describedby="img_input_help" id="img" type="file" name="img" accept="image/png, image/jpg, image/jpeg, image/gif" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none" required>
         </div>
         <button type="submit" class="w-full px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center bg-gray-900 text-white rounded-lg focus:ring-4 focus:ring-primary-200 hover:bg-primary-800">
             Ajouter
