34fa3d6489066c5dea42a7d493adb2fee37a2890
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-02-17 10:29:26 +0100

    Update 2023_12_21_200953_create_orders_table.php

--- database/migrations/2023_12_21_200953_create_orders_table.php
+++ database/migrations/2023_12_21_200953_create_orders_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->json('shipping_address');
             $table->json('amount');
             $table->integer('user_id')->nullable();
-            $table->boolean('shipped')->default(0);
+            $table->string('track_number')->nullable();
             $table->timestamps();
         });
     }
