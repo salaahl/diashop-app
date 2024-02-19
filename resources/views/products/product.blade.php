362d06fc3fea7b567195a9917e85a3b8ac5e80a0
salaahl <sokhona.salaha@gmail.com>
2024-02-19 01:43:41 +0100

    sync

--- resources/views/products/product.blade.php
+++ resources/views/products/product.blade.php
@@ -135,17 +135,9 @@
     @endforeach
 </section>
 <!-- Modal -->
-<div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-hidden fixed top-0 right-0 left-0 z-[999] justify-center items-center h-full w-full md:inset-0 max-h-full bg-black/50 backdrop-blur-[2px]">
+<div id="default-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-hidden fixed top-0 right-0 left-0 z-[999] justify-center items-center h-[100dvh] w-full md:inset-0 max-h-full bg-black/50 backdrop-blur-[2px]">
     <div class="relative max-h-full">
         <div class="relative shadow-2xl overflow-hidden">
-            <!-- Close modal button -->
-            <button type="button" class="absolute right-0 text-white bg-gray-800 rounded-full text-sm w-8 h-8 m-2 inline-flex justify-center items-center" data-modal-hide="default-modal">
-                <svg class="w-3 h-3" aria-hidden="false" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                </svg>
-                <span class="sr-only">Close modal</span>
-            </button>
-            <!-- Body -->
             <div>
                 <img src="/" id="modal-image" class="h-auto w-screen md:h-screen md:w-auto" alt="...">
             </div>
