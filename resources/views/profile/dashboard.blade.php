596a89dc5f841b45feea0d608324c61de1072463
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-03-26 17:07:12 +0100

    Update dashboard.blade.php

--- resources/views/profile/dashboard.blade.php
+++ resources/views/profile/dashboard.blade.php
@@ -18,7 +18,7 @@
 <h1>Mon profil</h1>
 <div class="min-h-[90vh] w-[100vw] absolute top-[10vh] left-0 right-0 flex flex-col justify-center items-center px-4">
     <h2 class="w-3/4 lg:w-2/4 mb-4 p-4 uppercase rounded-full bg-gray-200"><a href="{{ route('orders') }}">Mes commandes</a></h2>
-    <h2 class="w-3/4 lg:w-2/4 mb-4 p-4 uppercase rounded-full bg-gray-200"><a href="{{ route('favorites.show') }}">Mes favoris</a></h2>
+    <h2 class="w-3/4 lg:w-2/4 mb-4 p-4 uppercase rounded-full bg-gray-200"><a href="{{ route('favorites') }}">Mes favoris</a></h2>
     <h2 class="w-3/4 lg:w-2/4 mb-4 p-4 uppercase rounded-full bg-gray-200"><a href="{{ route('profile.edit') }}">Modifier mes informations</a></h2>
 </div>
 @endsection
