92cf8cc206b6165c93b4fad584441784505574cd
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-04-12 13:58:48 +0200

    Update banner-top.blade.php

--- resources/views/components/banner-top.blade.php
+++ resources/views/components/banner-top.blade.php
@@ -1,13 +1,13 @@
 <div id="sticky-banner" tabindex="-1" class="z-50 flex justify-between h-[30px] w-full text-white bg-gray-800">
   <div id="banner-inner" class="w-full flex flex-col flex-wrap mx-auto overflow-hidden">
     <p class="banner-slide banner-slide-first">
-      <span class="h-full">Profitez de -10% sur tout le site avec le code promo <strong>DIASHOP10</strong></span>
+      <span class="h-full">Profitez de -10% sur tout le site avec le code promo DIASHOP10</span>
     </p>
     <p class="banner-slide banner-slide-second">
-      <span class="h-full">Méthodes de paiement acceptées : CB, Paypal, Google Pay, Apple Pay</span>
+      <span class="h-full">Paiement en 3x sans frais avec Klarna</span>
     </p>
     <p class="banner-slide banner-slide-third">
-      <span class="h-full">Livraison offerte à partir de 50 euros</span>
+      <span class="h-full">Livraison offerte à partir de 50 euros d'achats</span>
     </p>
   </div>
 </div>
