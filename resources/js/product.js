7b78842b08c6fcbcbd0c6b688b073c24a5de84a9
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-04-13 16:54:33 +0200

    Update product.js

--- resources/js/product.js
+++ resources/js/product.js
@@ -47,8 +47,8 @@ if (window.innerWidth > 1023) {
                 style.backgroundSize =
                     imgWidth * 3 + "px " + imgHeight * 3 + "px";
                 style.backgroundRepeat = "no-repeat";
-                style.backgroundPositionX = xperc "%";
-                style.backgroundPositionY = yperc "%";
+                style.backgroundPositionX = xperc + "%";
+                style.backgroundPositionY = yperc + "%";
 
                 style.left = x - 180 + "px";
                 style.top = y - 180 + "px";
