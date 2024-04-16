6efcc608e5346e2d7531d219579f1f93994629ab
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-04-16 14:03:23 +0200

    Update product-card.blade.php

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -6,7 +6,7 @@
         </div>
         <div class="details lg:flex justify-between px-1 py-2 mt-1">
             <div class="w-full flex flex-wrap items-baseline justify-between">
-                <h4 class="title lg:max-w-[75%] text-ellipsis overflow-hidden">{{ ucfirst($title) }}</h4>
+                <h4 class="title lg:max-w-[75%] mr-[20px] text-ellipsis overflow-hidden">{{ ucfirst($title) }}</h4>
                 <div class="flex items-center">
                     @if(empty($message))
                     @if(empty($promotion))
