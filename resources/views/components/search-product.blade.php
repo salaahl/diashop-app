335635b4fe52a4bf6fcaee6f20b8c41caa9b9d23
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-09-28 10:39:29 +0200

    Update search-product.blade.php

--- resources/views/components/search-product.blade.php
+++ resources/views/components/search-product.blade.php
@@ -21,7 +21,7 @@
             <button type="submit" id="default-search-btn" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Voir plus</button>
         </div>
     </div>
-    <div id="search-results" class="h-full w-full flex flex-wrap lg:mt-12 max-lg:mt-10 overflow-auto lg:overflow-hidden"></div>
+    <div id="search-results" class="h-full w-full flex flex-wrap lg:mt-12 max-lg:mt-10 overflow-auto"></div>
     <div id="search-loader" class="hidden h-full w-full absolute left-0 right-0 justify-center items-center">
         <x-clothes-animation />
     </div>
