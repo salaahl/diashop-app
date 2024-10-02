25d0e3694831a34a45365f55361f4be253863f12
salaahl <sokhona.salaha@gmail.com>
2024-10-03 01:28:42 +0200

    ajustements

--- resources/views/components/banner-top.blade.php
+++ resources/views/components/banner-top.blade.php
@@ -1,4 +1,4 @@
-<div id="sticky-banner" tabindex="-1" class="z-50 flex justify-between h-[30px] w-full text-white bg-gray-800">
+<div id="sticky-banner" tabindex="-1" class="z-50 flex justify-between h-[30px] w-full text-white">
   <div id="banner-inner" class="w-full flex flex-col flex-wrap mx-auto overflow-hidden">
     <p class="banner-slide banner-slide-first">
       <span class="h-full">Profitez de -10% sur tout le site avec le code promo DIASHOP10</span>
