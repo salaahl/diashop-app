3a9aa4e249ca8fa62bba30a2ce631744d8204719
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-10-23 15:50:51 +0200

    Update search-product.blade.php

--- resources/views/components/search-product.blade.php
+++ resources/views/components/search-product.blade.php
@@ -18,7 +18,7 @@
                 </svg>
             </div>
             <input type="search" id="default-search" class="block w-full mt-5 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Rechercher un article" required>
-            <button type="submit" id="default-search-btn" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Voir plus</button>
+            <button type="submit" id="default-search-btn" class="text-white absolute end-2.5 bottom-2.5 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Voir plus</button>
         </div>
     </div>
     <div id="search-results" class="h-full w-full flex flex-wrap lg:mt-12 max-lg:mt-10 overflow-auto"></div>
