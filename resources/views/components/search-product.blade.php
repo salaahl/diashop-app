2d20b7599a7ccbd3b50741b0354cdc5f58973293
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2024-10-23 15:58:09 +0200

    Update search-product.blade.php

--- resources/views/components/search-product.blade.php
+++ resources/views/components/search-product.blade.php
@@ -1,24 +1,24 @@
 <section id="search-container" class="h-0 w-full flex flex-col justify-between absolute left-0 z-10 overflow-hidden">
     <div class="w-full">
         <div class="w-full flex justify-between mt-2">
-            <div class="w-[48%] flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
-                <input checked id="bordered-radio-1" type="radio" value="femme" name="catalog" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                <label for="bordered-radio-1" class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Catalogue Femme</label>
+            <div class="w-[48%] flex items-center ps-4 border border-gray-200 rounded">
+                <input checked id="bordered-radio-1" type="radio" value="femme" name="catalog" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2">
+                <label for="bordered-radio-1" class="w-full py-4 ms-2 text-sm font-medium text-gray-900">Catalogue Femme</label>
             </div>
-            <div class="w-[48%] flex items-center ps-4 border border-gray-200 rounded dark:border-gray-700">
-                <input id="bordered-radio-2" type="radio" value="homme" name="catalog" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                <label for="bordered-radio-2" class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Catalogue Homme</label>
+            <div class="w-[48%] flex items-center ps-4 border border-gray-200 rounded">
+                <input id="bordered-radio-2" type="radio" value="homme" name="catalog" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2">
+                <label for="bordered-radio-2" class="w-full py-4 ms-2 text-sm font-medium text-gray-900">Catalogue Homme</label>
             </div>
         </div>
-        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Rechercher</label>
+        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Rechercher</label>
         <div class="relative">
             <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                 </svg>
             </div>
-            <input type="search" id="default-search" class="block w-full mt-5 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Rechercher un article" required>
-            <button type="submit" id="default-search-btn" class="text-white absolute end-2.5 bottom-2.5 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Voir plus</button>
+            <input type="search" id="default-search" class="block w-full mt-5 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Rechercher un article" required>
+            <button type="submit" id="default-search-btn" class="text-white absolute end-2.5 bottom-2.5 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">Voir plus</button>
         </div>
     </div>
     <div id="search-results" class="h-full w-full flex flex-wrap lg:mt-12 max-lg:mt-10 overflow-auto"></div>
