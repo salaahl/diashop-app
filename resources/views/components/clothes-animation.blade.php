f21f52268ccc03fd12af677be562745cbd9d6932
salaahl <sokhona.salaha@gmail.com>
2024-10-31 23:42:52 +0100

    changement du loader

--- resources/views/components/clothes-animation.blade.php
+++ resources/views/components/clothes-animation.blade.php
@@ -1,25 +1,13 @@
 <div class="clothes-animation">
     <div class="container">
         <div class="carousel">
-            @for($i = 0; $i < 9; $i++)
-            <div class="jacket">
-                <img src="{{ asset('images/loader/jacket.png') }}" alt="jacket" />
-            </div>
-            @endfor
-        </div>
-    </div>
-    <div class="container">
-        <div class="carousel">
-            @for($i = 0; $i < 9; $i++)
+            @for($i = 0; $i <= 20; $i++)
             <div class="dress">
                 <img src="{{ asset('images/loader/dress.png') }}" alt="dress" />
             </div>
-            @endfor
-        </div>
-    </div>
-    <div class="container">
-        <div class="carousel">
-            @for($i = 0; $i < 9; $i++)
+            <div class="jacket">
+                <img src="{{ asset('images/loader/jacket.png') }}" alt="jacket" />
+            </div>
             <div class="pant">
                 <img src="{{ asset('images/loader/pant.png') }}" alt="pant" />
             </div>
