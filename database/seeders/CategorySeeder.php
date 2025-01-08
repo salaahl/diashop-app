719fea225d2b4fc4d73dc5999f8f93f27f6304ae
Salaha Sokhona <67805036+salaahl@users.noreply.github.com>
2025-01-08 15:19:28 +0100

    Update CategorySeeder.php

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -12,15 +12,32 @@ class CategorySeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
+    {    
+        // Catalogues pour femmes
         Category::factory()->create([
             'name' => 'foulards',
+            'catalog_id' => 1,
+        ]);
+        Category::factory()->create([
+            'name' => 'vestes',
+            'catalog_id' => 1,
+        ]);
+        Category::factory()->create([
+            'name' => 't-shirts',
             'catalog_id' => 1,
         ]);
 
         // Catalogues pour hommes
         Category::factory()->create([
             'name' => 'vestes',
+            'catalog_id' => 2,
+        ]);
+        Category::factory()->create([
+            'name' => 'sacs',
+            'catalog_id' => 2,
+        ]);
+        Category::factory()->create([
+            'name' => 'chemises',
             'catalog_id' => 2,
         ]);
     }
