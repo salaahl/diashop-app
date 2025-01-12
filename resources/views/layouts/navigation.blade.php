654594b34f1f82b765c78cc847dbf08887c2317b
Salaha Sokhona <sokhona.salaha@gmail.com>
2025-01-12 17:00:04 +0000

    style : ajustements

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,7 +2,6 @@
     <div class="h-full w-full flex flex-wrap items-center justify-between mx-auto xl:p-0">
         <a href="/" id="logo" class="h-full flex flex-col justify-center items-center absolute top-0 left-2/4 right-2/4">
             <span class="self-center uppercase font-semibold whitespace-nowrap">{{ env("APP_NAME") }}</span>
-            <span class="self-center m-0 text-sm uppercase whitespace-nowrap">Paris</span>
         </a>
         <div class="has-mobile-nav-btn flex lg:hidden">
             <button
