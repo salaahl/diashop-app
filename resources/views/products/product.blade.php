7b29a90bb6c5fb16ea8e969a884b15eaa95e7e7b
salaahl <sokhona.salaha@gmail.com>
2025-08-01 22:33:42 +0200

    style: modifications mineures

--- resources/views/products/product.blade.php
+++ resources/views/products/product.blade.php
@@ -203,7 +203,7 @@
     <div class="carousel-container">
         <div id="slides-container" class="relative max-h-full">
             <div class="relative shadow-2xl">
-                <button type="button" class="absolute top-1 right-1 md:top-2 md:right-2 z-[5] text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
+                <button type="button" class="absolute top-1 right-1 md:top-2 md:right-2 z-[5] text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-full text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white transition-ease-in-out duration-200" data-modal-hide="default-modal">
                     <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                     </svg>
