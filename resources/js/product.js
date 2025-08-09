ac2622f1a514c3fffafae97a407ee17a3712d08b
salaahl <sokhona.salaha@gmail.com>
2025-08-09 19:14:57 +0200

    revert

--- resources/js/product.js
+++ resources/js/product.js
@@ -7,12 +7,6 @@ const popUp = $("#popup");
 const popUpTimer = 5000;
 let url = window.location.href;
 
-console.log(import.meta.env.VITE_TEST);
-console.log(import.meta.env.TEST);
-console.log(import.meta.env.STANDARD_DELIVERY_CHARGES);
-console.log(import.meta.env);
-console.log(process.env.STANDARD_DELIVERY_CHARGES);
-
 // Loupe pour les photos d'articles en mode PC
 if (window.innerWidth > 1023) {
     let zoomer = (function () {
